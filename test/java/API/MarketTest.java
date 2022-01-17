538f06147a148d4a5f47a2d6104e16a14789d6d3
DanielPintto <75393664+DanielPintto@users.noreply.github.com>
2022-01-17 19:39:09 +0000

    Add files via upload

--- test/java/API/MarketTest.java
+++ test/java/API/MarketTest.java
@@ -1,97 +1,148 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+/**
+ * Epoca Normal ED
+ * Daniel Pinto 8200412
+ * Tomás Pendão 8170308
  */
 package API;
 
 import Collections.LinkedList.LinkedQueue;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 
 /**
+ * Classe utilizada para realizar testes à classe Market.
  *
  * @author Daniel Pinto
  */
 public class MarketTest {
-    
+
     Market mercado;
-    
+
+    /**
+     * Constrói um mercado para a realização de testes.
+     */
     @Before
     public void setUp() {
-         System.out.println("Running set up");
+        System.out.println("Running set up");
+
         this.mercado = new Market("mercado1");
-    }
-    
-    @After
-    public void tearDown() {
-        System.out.println("Running tear down");
-        this.mercado=null;
     }
 
     /**
-     * Test of addClients method, of class Market.
+     * Apaga um mercado após a realização de testes.
+     */
+    @After
+    public void tearDown() {
+        System.out.println("Running tear down");
+
+        this.mercado = null;
+    }
+
+    /**
+     * Testa o método addCilents da classe Market.
      */
     @Test
     public void testAddClients() {
         System.out.println("addClients");
+
         float demand = 0.0F;
         Market instance = new Market();
-        instance.addClients(demand);
+        instance.addClient(demand);
+
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
 
     /**
-     * Test of removeClient method, of class Market.
+     * Testa o método removeClient da classe Market.
      */
     @Test
     public void testRemoveClient() {
         System.out.println("removeClient");
+
         Market instance = new Market();
         boolean expResult = false;
         boolean result = instance.removeClient();
         assertEquals(expResult, result);
+
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
 
     /**
-     * Test of printClients method, of class Market.
+     * Testa o método printClients da classeMarket.
      */
     @Test
     public void testPrintClients() {
         System.out.println("printClients");
+
         Market instance = new Market();
         String expResult = "";
         String result = instance.printClients();
         assertEquals(expResult, result);
+
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
 
     /**
-     * Test of export method, of class Market.
+     * Testa o método exportJSON da classe Market.
      */
     @Test
-    public void testExport() {
-       System.out.println("export");
-        assertTrue(this.mercado.export());
+    public void testExportJSON() {
+        System.out.println("export");
+
+        assertTrue(this.mercado.exportJSON());
     }
 
     /**
-     * Test of getClients method, of class Market.
+     * Testa o método getClients da classe Market.
      */
     @Test
     public void testGetClients() {
         System.out.println("getClients");
+
         Market instance = new Market();
         LinkedQueue expResult = null;
         LinkedQueue result = instance.getClients();
         assertEquals(expResult, result);
+
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
+
+    /**
+     * Testa o método addClient da classe Market.
+     */
+    @Test
+    public void testAddClient() {
+        System.out.println("addClient");
+
+        float demand = 0.0F;
+        Market instance = new Market();
+        instance.addClient(demand);
+
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Testa o método importJSON da classe Market.
+     */
+    @Test
+    public void testImportJSON() {
+        System.out.println("importJSON");
+
+        Market instance = new Market();
+        boolean expResult = false;
+        boolean result = instance.importJSON();
+        assertEquals(expResult, result);
+
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
 }
