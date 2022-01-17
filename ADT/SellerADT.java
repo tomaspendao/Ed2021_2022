04498e0c64151f1cc71805e199fb359427805656
DanielPintto <75393664+DanielPintto@users.noreply.github.com>
2022-01-17 11:04:00 +0000

    Add files via upload

--- ADT/SellerADT.java
+++ ADT/SellerADT.java
@@ -6,15 +6,40 @@
 package ADT;
 
 /**
- * @author 
+ * SellerADT define uma interface com comportamentos de um vendedor.
+ *
+ * @author Daniel Pinto
  */
 public interface SellerADT {
-    
-    public boolean addMarkets();
 
-    public void editCapacity();
+    /**
+     * Adiciona um mercado com um determinado nome a um vendedor.
+     *
+     * @param name Nome do mercado
+     */
+    public void addMarket(String name);
 
-    public String printSeller();
+    /**
+     * Método utilizado para editar a capacidade que um vendedor pode
+     * transportar. Apenas será alterada para um valor válido,superior a 0
+     * (zero).
+     *
+     * @param newCapacity Nova capacidade que o vendedor consegue transportar.
+     * @return true caso a nova capacidade seja definida com sucesso, false caso
+     * contrário.
+     */
+    public boolean editCapacity(float newCapacity);
 
+    /**
+     * Método utilizado para representar informações relativas a um vendedor.
+     */
+    public void printSeller();
+
+    /**
+     * Exporta os dados de um vendedor para formato JSON.
+     *
+     * @return true caso seja possível exportar para formato JSON, false caso
+     * contrário.
+     */
     public boolean export();
 }
