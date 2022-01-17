36237d69d3e1df5331552f3e0c7afc8294d64568
DanielPintto <75393664+DanielPintto@users.noreply.github.com>
2022-01-17 20:04:03 +0000

    Add files via upload

--- ADT/RouteADT.java
+++ ADT/RouteADT.java
@@ -6,12 +6,24 @@
 package ADT;
 
 /**
- * @author 
+ * RouteADT define uma interface com comportamentos de uma rota.
+ *
+ * @author Tomás Pendão
  */
 public interface RouteADT {
+
+    /**
+     * Método utilizado para criar uma rota.
+     */
     public void generateRoute();
 
     public void refillRoute();
 
+    /**
+     * Exporta os dados de um mercado para formato JSON.
+     *
+     * @return true caso seja possível exportar para formato JSON, false caso
+     * contrário.
+     */
     public boolean export();
 }
