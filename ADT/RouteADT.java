8ff59df6769aae255106d3a88d8bfae887aa1a2d
tomaspendao <39770209+tomaspendao@users.noreply.github.com>
2022-01-18 17:52:27 +0000

    Add files via upload

--- ADT/RouteADT.java
+++ ADT/RouteADT.java
@@ -5,25 +5,34 @@
  */
 package ADT;
 
+import API.Seller;
+import java.util.Iterator;
+
 /**
  * RouteADT define uma interface com comportamentos de uma rota.
  *
  * @author Tomás Pendão
  */
 public interface RouteADT {
+    
+    //getProduct > 0 
+    //genrerateRoute(){}
+    //
+    /**
+     * Método utilizado para criar uma rota para um vendedor.
+     * 
+     * @param vendedor Vendedor para qual vai ser gerada a rota de entregas.
+     * @param marketList Lista de mercados que o vendedor tem de visitar.
+     * @return rota com o caminho.
+     */
+    public Iterator generateRoute(Seller vendedor,UnorderedListADT<String> marketList); //a,b,x abcdx s->a s->y->a     a->b a->x  |a|b|x| -> lista [a|b|x]
 
     /**
-     * Método utilizado para criar uma rota.
+     * Metodo utilizado para reabastecer um vendedor.
+     * 
+     * @param vendedor Vendedor que vai ter de reabastecer num armazém.
+     * @return rota com o caminho.
      */
-    public void generateRoute();
+    public Iterator refillRoute(Seller vendedor); //  stock>=soma  |a|b|c| ex: s-a -> 60  s-b 30 
 
-    public void refillRoute();
-
-    /**
-     * Exporta os dados de um mercado para formato JSON.
-     *
-     * @return true caso seja possível exportar para formato JSON, false caso
-     * contrário.
-     */
-    public boolean export();
 }
