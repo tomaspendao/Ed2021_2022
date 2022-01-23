8fc2181e681e690165333c536be320949093b865
tomaspendao <39770209+tomaspendao@users.noreply.github.com>
2022-01-23 01:57:09 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+import API.Menu;
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Tomás Pendão
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu.start();
+    }
+    
+}
