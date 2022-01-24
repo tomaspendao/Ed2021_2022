d654695807f82ef79f4b82b906774d20223c4ea3
tomaspendao <39770209+tomaspendao@users.noreply.github.com>
2022-01-24 18:29:50 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -17,6 +17,16 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /**
+         * To-Do
+         * Acabar os tests //done
+         * meter o.jar a dar?
+         * 
+         * Melhorar os imports e os prints
+         * fazer os metodos das routes e adicionar essas opções ao menu
+         * 
+         */
+        
         Menu.start();
     }
     
