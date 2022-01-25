e8ae31e0a514efcef844be001ff791540e91a93a
DanielPintto <75393664+DanielPintto@users.noreply.github.com>
2022-01-25 15:30:34 +0000

    Add files via upload

--- Exceptions/InvalidValueException.java
+++ Exceptions/InvalidValueException.java
@@ -1,10 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * Epoca Normal ED
+ * Daniel Pinto 8200412
+ * Tomás Pendão 8170308
  */
 package Exceptions;
 
 /**
+ * Classe para armazenar as várias exceções que podem ocorrer com o
+ * funcionamento da API.
  *
  * @author Tomás Pendão
  */
@@ -13,7 +16,7 @@ public class InvalidValueException extends RuntimeException {
     /**
      * Sets up this exception with an appropriate message.
      *
-     * @param element the name of the element
+     * @param value Valor que não é correto.
      */
     public InvalidValueException(String value) {
         super(value + " is not a valid value.");
