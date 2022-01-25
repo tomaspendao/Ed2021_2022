0a05dda9993a8b2cb1aae0c42cc623b520d3276c
DanielPintto <75393664+DanielPintto@users.noreply.github.com>
2022-01-25 15:35:05 +0000

    Add files via upload

--- API/Route.java
+++ API/Route.java
@@ -1,49 +1,89 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * Epoca Normal ED
+ * Daniel Pinto 8200412
+ * Tomás Pendão 8170308
  */
 package API;
 
 import ADT.UnorderedListADT;
 import Collections.DoubleLinkedList.DoubleLinkedUnorderedList;
-import Collections.LinkedList.GraphWeightList;
-import java.util.Iterator;
 
 /**
+ * Classe utilizada para armazenar dados e comportamentos de uma rota.
  *
  * @author Tomás Pendão
  */
 public class Route {
-    
-    private UnorderedListADT<Place> rota; 
+
+    /**
+     * Rota a percorrer.
+     */
+    private UnorderedListADT<Place> rota;
+
+    /**
+     * Número de vezes em que foi preciso reabastecer.
+     */
     private int amountOfRefills;
+
+    /**
+     * Distância total.
+     */
     private float totalDistance;
+
+    /**
+     * Vendedor que irá percorrer a rota.
+     */
     private Seller vendedor;
+
+    /**
+     * Stock do vendedor.
+     */
     private float stock;
-    
+
     //e capaz de nao ser necessario
     private Place start;
     private Place target;
 
+    /**
+     * Construtor que cria uma instância de Route. A rota, número de
+     * reabastecimentos, distância total, stock e destino são inicializados a 0
+     * (zero) ou null. O ponto de partida é sempre a sede.
+     *
+     * @param vendedor Vendedor que irá percorrer a rota.
+     */
     public Route(Seller vendedor) {
         this.rota = new DoubleLinkedUnorderedList<>();
         this.amountOfRefills = 0;
         this.totalDistance = 0;
-        this.vendedor = vendedor; 
+        this.vendedor = vendedor;
         this.stock = 0;
-        //
         this.start = null;//sede
         this.target = null;
     }
 
+    /**
+     * Método utilizado para obter uma rota.
+     *
+     * @return Rota.
+     */
     public UnorderedListADT<Place> getRota() {
         return rota;
     }
 
+    /**
+     * Método utilizado para estabelecer uma rota.
+     *
+     * @param rota Rota a estabelecer.
+     */
     public void setRota(UnorderedListADT<Place> rota) {
         this.rota = rota;
     }
 
+    /**
+     * Método utilizado para obter o número de reabastecimentos.
+     *
+     * @return Número de reabastecimentos.
+     */
     public int getAmountOfRefills() {
         return amountOfRefills;
     }
@@ -51,7 +91,7 @@ public class Route {
     public void setAmountOfRefills(int amountOfRefills) {
         this.amountOfRefills = amountOfRefills;
     }
-    
+
     public void incrementAmountOfRefills() {
         this.amountOfRefills++;
     }
@@ -63,7 +103,7 @@ public class Route {
     public void setTotalDistance(float totalDistance) {
         this.totalDistance = totalDistance;
     }
-    
+
     public void addTotalDistance(float plusDistance) {
         this.totalDistance = this.totalDistance + plusDistance;
     }
@@ -83,11 +123,11 @@ public class Route {
     public void setStock(float stock) {
         this.stock = stock;
     }
-    
+
     public void addStock(float plusStock) {
         this.stock = this.stock + plusStock;
     }
-    
+
     public void removeStock(float minusStock) {
         this.stock = this.stock - minusStock;
     }
