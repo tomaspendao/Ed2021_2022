264e6c290c12377206e5249f5bb0c258ec240e61
DanielPintto <75393664+DanielPintto@users.noreply.github.com>
2022-01-26 12:03:20 +0000

    Add files via upload

--- test/java/API/CompanyTest.java
+++ test/java/API/CompanyTest.java
@@ -1,19 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+/**
+ * Epoca Normal ED
+ * Daniel Pinto 8200412
+ * Tomás Pendão 8170308
  */
 package API;
 
-import ADT.UnorderedListADT;
 import Collections.DoubleLinkedList.DoubleLinkedUnorderedList;
 import Collections.LinkedList.GraphWeightList;
 import Models.Nodes.ArestaWeight;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
+ * Classe utilizada para realizar testes à classe Company.
  *
  * @author Tomás Pendão
  */
@@ -24,18 +23,24 @@ public class CompanyTest {
     DoubleLinkedUnorderedList<Place> places = new DoubleLinkedUnorderedList<>();
     GraphWeightList<Place> map = new GraphWeightList<>();
 
+    /**
+     * Constrói uma empresa para a realização de testes.
+     */
     public void setUp() {
         System.out.println("Running Set Up");
         this.empresa = new Company(sellers, places, map, "empresa");
     }
 
+    /**
+     * Apaga uma empresa após a realização de testes.
+     */
     public void tearDown() {
         System.out.println("Running Tear Down");
         this.empresa = null;
     }
 
     /**
-     * Test of addSeller method, of class Company.
+     * Testa o método addSeller da classe Company.
      */
     @Test
     public void testAddSeller() {
@@ -51,7 +56,7 @@ public class CompanyTest {
     }
 
     /**
-     * Test of editSeller method, of class Company.
+     * Testa o método editSeller da classe Company.
      */
     @Test
     public void testEditSeller() {
@@ -70,7 +75,7 @@ public class CompanyTest {
     }
 
     /**
-     * Test of addMarket method, of class Company.
+     * Testa o método addMarket da classe Company.
      */
     @Test
     public void testAddMarket() {
@@ -88,7 +93,7 @@ public class CompanyTest {
     }
 
     /**
-     * Test of editMarket method, of class Company.
+     * Testa o método editMarket da classe Company.
      */
     @Test
     public void testEditMarket() {
@@ -103,7 +108,7 @@ public class CompanyTest {
     }
 
     /**
-     * Test of addWarehouse method, of class Company.
+     * Testa o método addWarehouse da classe Company.
      */
     @Test
     public void testAddWarehouse() {
@@ -122,7 +127,7 @@ public class CompanyTest {
     }
 
     /**
-     * Test of editWarehouse method, of class Company.
+     * Testa o método editWarehouse da classe Company.
      */
     @Test
     public void testEditWarehouse() {
@@ -138,7 +143,7 @@ public class CompanyTest {
     }
 
     /**
-     * Test of addRoute method, of class Company.
+     * Testa o método addRoute da classe Company.
      */
     @Test
     public void testAddRoute() {
@@ -155,7 +160,7 @@ public class CompanyTest {
     }
 
     /**
-     * Test of editRoute method, of class Company.
+     * Testa o método editRoute da classe Company.
      */
     @Test
     public void testEditRoute() {
@@ -165,17 +170,17 @@ public class CompanyTest {
         this.empresa.addRoute("empresa", "Armazem1", 100);
         DoubleLinkedUnorderedList<ArestaWeight>[] path = this.map.getAdjList();
         System.out.println("editRoute");
-        
-        this.empresa.editRoute("empresa","Armazem1",12);
+
+        this.empresa.editRoute("empresa", "Armazem1", 12);
         assertEquals(0, path[0].first().getStart());
         assertEquals(1, path[0].first().getTarget());
         assertEquals(12, path[0].first().getWeight(), 0);
-        
+
         this.tearDown();
     }
 
     /**
-     * Test of printSellers method, of class Company.
+     * Testa o método printSellers da classe Company.
      */
     @Test
     public void testPrintSellers() {
@@ -191,7 +196,7 @@ public class CompanyTest {
     }
 
     /**
-     * Test of printMarket method, of class Company.
+     * Testa o método printMarket da classe Company.
      */
     @Test
     public void testPrintMarket() {
@@ -199,12 +204,12 @@ public class CompanyTest {
         System.out.println("printMarket");
         Market market1 = new Market("mercado1");
         this.empresa.addMarket(market1);
-        assertEquals("mercado1;",this.empresa.printMarket());
+        assertEquals("mercado1;", this.empresa.printMarket());
         this.tearDown();
     }
 
     /**
-     * Test of printWarehouse method, of class Company.
+     * Testa o método printWarehouse da classe Company.
      */
     @Test
     public void testPrintWarehouses() {
@@ -212,12 +217,12 @@ public class CompanyTest {
         Warehouse warehouse = new Warehouse(200, 100, "Armazem1");
         this.empresa.addWarehouse(warehouse);
         System.out.println("printWarehouse");
-        assertEquals("Armazem1;",this.empresa.printWarehouses());
+        assertEquals("Armazem1;", this.empresa.printWarehouses());
         this.tearDown();
     }
 
     /**
-     * Test of export method, of class Company.
+     * Testa o método export da classe Company.
      */
     @Test
     public void testExport() {
@@ -260,39 +265,38 @@ public class CompanyTest {
     }
 
     /**
-     * Test of addMarketToSeller method, of class Company.
+     * Testa o método addMarketToSeller da classe Company.
      */
     @Test
     public void testAddMarketToSeller() {
         this.setUp();
-        
+
         Seller seller1 = new Seller(10, 1, "Ricardo");
         this.empresa.addSeller(seller1);
         this.empresa.addSeller(seller1);
         this.empresa.addSeller(new Seller(10, 1, "José"));
         this.empresa.addSeller(new Seller(10, 2, "Mario"));
-        
+
         Market market1 = new Market("mercado1");
         this.empresa.addMarket(market1);
-        
+
         System.out.println("addMarketToSeller");
         this.empresa.addMarketToSeller(1, "mercado1");
         assertEquals("mercado1", this.sellers.first().getMercados_a_visitar().first());
         this.tearDown();
     }
-    
+
     /**
-     * Test of import method, of class Company.
+     * Testa o método import da classe Company.
      */
     @Test
     public void testImport() {
         this.setUp();
         Company empresa = Company.importCompany("Company_empresaCR.json");
         empresa.export();
-        
+
         //System.out.println(this.empresa.getLocais().first().getName());
         //System.out.println(this.empresa.getLocais().first().getType());
-        
         this.tearDown();
     }
 
