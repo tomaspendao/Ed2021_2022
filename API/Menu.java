2ac8eaf9baeb43ec0c57e68696ff61fbab868a55
tomaspendao <39770209+tomaspendao@users.noreply.github.com>
2022-02-01 16:39:32 +0000

    Update Menu.java

--- API/Menu.java
+++ API/Menu.java
@@ -12,6 +12,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Classe para armazenar comportamentos de um menu.
