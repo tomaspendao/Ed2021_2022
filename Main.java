93d1cdf43ff98bcef9c6194e3a73cc25a99a22b2
tomaspendao <39770209+tomaspendao@users.noreply.github.com>
2022-02-01 12:32:30 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 
 import API.Menu;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Epoca Normal ED Daniel Pinto 8200412 Tomás Pendão 8170308
@@ -14,7 +17,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /**
          * To-Do Acabar os tests //done meter o.jar a dar?
          *
@@ -22,8 +25,15 @@ public class Main {
          * adicionar essas opções ao menu
          *
          */
+        //System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        //mostra os nomes dos ficheiros .json de export da empresa
+
+        File file = new File("./exportJSON/empresa");//Creating the directory
+        File file2 = new File("./exportJSON/vendedor");//Creating the directory
+        File file3 = new File("./exportJSON/mercado");//Creating the directory
+        File file4 = new File("./exportJSON/armazem");//Creating the directory
 
         Menu.start();
-    }
 
+    }
 }
