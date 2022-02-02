17a001446dd71239557ecff000a6e3091be4b0c8
DanielPintto <75393664+DanielPintto@users.noreply.github.com>
2022-02-02 16:29:26 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 
+/**
+ * Epoca Normal ED
+ * Daniel Pinto 8200412
+ * Tomás Pendão 8170308
+ */
 import API.Menu;
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 
-/**
- * Epoca Normal ED Daniel Pinto 8200412 Tomás Pendão 8170308
- */
 /**
  * Classe que irá invocar um menu. É o ponto de partida do programa.
  *
@@ -18,22 +19,12 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        /**
-         * To-Do Acabar os tests //done meter o.jar a dar?
-         *
-         * Melhorar os imports e os prints fazer os metodos das routes e
-         * adicionar essas opções ao menu
-         *
-         */
-        //System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        //mostra os nomes dos ficheiros .json de export da empresa
 
         File file = new File("./exportJSON/empresa");//Creating the directory
-        File file2 = new File("./exportJSON/vendedor");//Creating the directory
-        File file3 = new File("./exportJSON/mercado");//Creating the directory
-        File file4 = new File("./exportJSON/armazem");//Creating the directory
+        file = new File("./exportJSON/vendedor");//Creating the directory
+        file = new File("./exportJSON/mercado");//Creating the directory
+        file = new File("./exportJSON/armazem");//Creating the directory
 
         Menu.start();
-
     }
 }
