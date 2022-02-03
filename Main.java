cb9771d65e935106b82a862883b45b27c1d85aeb
tomaspendao <39770209+tomaspendao@users.noreply.github.com>
2022-02-03 01:18:08 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,6 +17,7 @@ public class Main {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
 
